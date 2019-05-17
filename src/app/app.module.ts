2ce8cf24d8dcca8ce00212a634912daf7a5cc698
alumno <alumno@utnfra.com.ar>
2019-05-17 12:24:02 -0300

    subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FileDropModule } from 'ngx-file-drop';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubidaComponent } from './componentes/subida/subida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubidaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/subida/subida.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry, FileDropModule } from 'ngx-file-drop';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-subida',
+  templateUrl: './subida.component.html',
+  styleUrls: ['./subida.component.css']
+})
+export class SubidaComponent implements OnInit {
+  public files: UploadFile[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  public dropped(event: UploadEvent) {
+    this.files = event.files;
+    for (const droppedFile of event.files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+
+          // You could upload it like this:
+          const formData = new FormData();
+          formData.append('imagen', file, droppedFile.relativePath);
+          formData.append('dato', 'datos');
+          //formData.append();
+
+          // Headers
+/*           const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          }); */
+
+          this.http.post('http://localhost/2pp/media/altaFoto', formData, {})
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          });
+
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event){
+    console.log(event);
+  }
+
+  public fileLeave(event){
+    console.log(event);
+  }
+}
